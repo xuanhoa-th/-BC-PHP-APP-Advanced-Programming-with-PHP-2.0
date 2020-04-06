4701b308419e71c347675158c3c54842b234c558
hkbike <xuanhoa93th@gmail.com>
2020-04-06 09:02:27 +0700

    sdfsfs

--- BT/BT - Gop-mang/index.php
+++ BT/BT - Gop-mang/index.php
@@ -10,22 +10,10 @@
 		$arr2 = [ 6,12,98,102,7,4];
 		$arr3 = [];
 
-		$arr3 = array_merge($arr1,$arr3);//gop mang
-
-		foreach ($arr2 as $value) {
-			$arr4 =	array_push($array3,$value);
-				echo '<pre>';
-					print_r($arr4)	;		
-				echo '<pre>';			
-		}
-		// for ($i = 0; $i <= count($arr2) ; $i++) {
-		// 	array_push($array3,$arr2[$i]);
-		// 	echo '<pre>';
-		// 			print_r($array3)	;		
-		// 		echo '<pre>';	
-		// }
-		
-
+		$arr3 = array_merge($arr1,$arr2);//gop mang
+		echo '<pre>';
+					print_r($arr3)	;		
+		echo '<pre>';
 	 ?>
 </body>
 </html>
--- Z.testPHP/index.php
+++ Z.testPHP/index.php
@@ -1,10 +1,21 @@
-<?php 
-
-$input = range(0,10);
-array_splice($input, 1, ["22"]);
- 
-echo "<pre>";
-    print_r($input);
-echo "</pre>";
-
- ?>
+<?php
+class CustomException2 extends Exception {
+    public function orrorMessage(){
+        $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+            .': <b>'.$this->getMessage().'</b> is not a valid E-Mail address.';
+        return $errormsg;
+    }
+}
+$email = "someone@example.com";
+try {
+    if (filter_var($email,FILTER_VALIDATE_EMAIL) === FALSE){
+        throw new CustomException2($email);
+    }
+}
+catch (CustomException2 $e){
+    echo $e ->orrorMessage();
+}
+catch (Exception $e){
+    echo $e ->getMessage();
+}
+?>
