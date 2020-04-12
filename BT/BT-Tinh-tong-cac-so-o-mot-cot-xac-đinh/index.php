7bee6762c36435bcb61ed8fa6e81a3eac62dc871
hkbike <xuanhoa93th@gmail.com>
2020-04-12 22:11:50 +0700

    sdfs

--- BT/BT-Tinh-tong-cac-so-o-mot-cot-xac-đinh/index.php
+++ BT/BT-Tinh-tong-cac-so-o-mot-cot-xac-đinh/index.php
@@ -1,3 +1,16 @@
-<?php 
-echo "chua lam";
- ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<form action="view/matran.php" method="GET">
+		Nhap vao do dai ma tran vuong:
+		<input type="text" name="size">
+		<button type="submit">hien thi ma tran VUONG</button>
+	</form>
+</body>
+</html>
---
+++ BT/BT-Tinh-tong-cac-so-o-mot-cot-xac-đinh/view/matran.php
@@ -0,0 +1,57 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	$size = $_REQUEST['size'];
+};
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	$size = $_REQUEST['size'];
+	$adres = $_REQUEST['adres'];
+    $arr = [];
+
+    for ($n = 0; $n < $size; $n++){
+        $arr[$n] = [];
+        for ($m = 0 ; $m < $size ; $m++){
+            $arr[$n][$m] = $_REQUEST['item-'. $n .'-'. $m];
+        }
+    }
+
+	function sum($arr){
+        $sum = 0;
+		for ($i = 0; $i < count($arr) ; $i++) {
+			for ($j = 0; $j < count($arr[$i]); $j++ ){
+			    
+			        $sum += $arr[$i][$j];
+               
+            }
+		}
+		return $sum;
+	}
+	echo "tong la".sum($arr);
+
+}
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>max</title>
+ </head>
+ <body>
+ 	<form method="POST">
+ 		<table>
+ 			<?php for ($i = 0; $i < $size ; $i++):?>
+ 				<tr>
+				<?php for ($j = 0; $j < $size ; $j++):?>
+				    <td><input type="text" name="item-<?php echo $i.'-'.$j  ?>"></td>
+ 			    <?php endfor; ?>
+ 			    </tr>
+ 			<?php endfor; ?>
+ 		<!-- Nhap vao vi tri cot:
+		<input type="text" name="adres"> -->
+ 		</table>
+		<button type="submit">Tinh tong</button>
+ 	</form>
+ </body>
+ </html>
